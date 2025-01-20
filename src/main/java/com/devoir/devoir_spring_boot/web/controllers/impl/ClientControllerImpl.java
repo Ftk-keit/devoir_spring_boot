b9d3b103f1fa1d5f002deefad75f6d397b7c6c52
fakeita <fatimakeite05@gmail.com>
2025-01-20 15:50:09 +0000

    devoir

--- src/main/java/com/devoir/devoir_spring_boot/web/controllers/impl/ClientControllerImpl.java
+++ src/main/java/com/devoir/devoir_spring_boot/web/controllers/impl/ClientControllerImpl.java
@@ -3,6 +3,7 @@ package com.devoir.devoir_spring_boot.web.controllers.impl;
 import com.devoir.devoir_spring_boot.data.entities.Client;
 import com.devoir.devoir_spring_boot.services.ClientService;
 import com.devoir.devoir_spring_boot.web.controllers.ClientController;
+import com.devoir.devoir_spring_boot.web.dto.repuest.ClientRequest;
 import com.devoir.devoir_spring_boot.web.dto.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +19,8 @@ public class ClientControllerImpl implements ClientController {
     }
 
     @Override
-    public ResponseEntity<Response> createClientWithCommandes(@Valid @RequestBody  Client client) {
-        Client client1 = clientService.save(client);
+    public ResponseEntity<Response> createClientWithCommandes(@Valid @RequestBody ClientRequest client) {
+        Client client1 = clientService.save(client.ToEntity(client)));
         //regle pour valider.......
         return new ResponseEntity<>(new Response("201", "Clients", client1), HttpStatus.CREATED) ;
     }
--- src/main/java/com/devoir/devoir_spring_boot/web/dto/repuest/ClientRequest.java
+++ src/main/java/com/devoir/devoir_spring_boot/web/dto/repuest/ClientRequest.java
@@ -1,12 +1,56 @@
 package com.devoir.devoir_spring_boot.web.dto.repuest;
 
+import com.devoir.devoir_spring_boot.data.entities.Client;
+import com.devoir.devoir_spring_boot.data.entities.Commande;
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.processing.Pattern;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClientRequest {
     private Long id;
 
-
+    @NotNull
     private String name;
 
-
+    @Column(length = 25)
+    @NotNull(message = "Le numéro de téléphone ne doit pas être null")
+    @NotBlank(message = "Le numéro de téléphone ne doit pas être vide")
+    @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Numéro de téléphone invalide")
+    @Column(unique = true, nullable = false)
     private String telephone;
 
+    public Client ToEntity(Client data) {
+        Client entity = new Client();
+        entity.setName(data.getName());
+        entity.setTelephone(data.getTelephone());
+
+    return entity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
